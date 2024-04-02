c10fc3c701e04b59f95ea1361b13957254601c83
diniamo <diniamo69@gmail.com>
2024-04-02 17:34:39 +0200

    feat: improve format_rule(s) macro

--- src/lib.rs
+++ src/lib.rs
@@ -21,27 +21,34 @@ pub fn get_ruleset_from_workspace<'a>(
         .find(|r| r.workspace_string == workspace.id.to_string())
 }
 
-macro_rules! format_rule {
-    ($vector:expr, $final_name:expr, $value:expr) => {{
-        if $value.is_some() {
-            $vector.push(format!("{}:{}", $final_name, $value.unwrap()));
-        }
-    }};
+macro_rules! format_rules {
+    ($vector:tt, [$(($final_name:expr, $value:expr)),+ $(,)?]) => {
+        $(
+            if $value.is_some() {
+                $vector.push(format!("{}:{}", $final_name, $value.unwrap()));
+            }
+        )*
+    };
 }
 
 pub fn format_for_command(ruleset: &WorkspaceRuleset) -> String {
     let mut vector = Vec::new();
 
-    format_rule!(vector, "monitor", ruleset.monitor.as_ref());
-    format_rule!(vector, "default", ruleset.default);
-    format_rule!(vector, "gapsin", ruleset.gaps_in.clone().map(|s| s[0]));
-    format_rule!(vector, "gapsout", ruleset.gaps_out.clone().map(|s| s[0]));
-    format_rule!(vector, "bordersize", ruleset.border_size);
-    format_rule!(vector, "border", ruleset.border);
-    format_rule!(vector, "shadow", ruleset.shadow);
-    format_rule!(vector, "rounding", ruleset.rounding);
-    format_rule!(vector, "decorate", ruleset.decorate);
-    format_rule!(vector, "persistent", ruleset.persistent);
+    format_rules!(
+        vector,
+        [
+            ("monitor", ruleset.monitor.as_ref()),
+            ("default", ruleset.default),
+            ("gapsin", ruleset.gaps_in.clone().map(|s| s[0])),
+            ("gapsout", ruleset.gaps_out.clone().map(|s| s[0])),
+            ("bordersize", ruleset.border_size),
+            ("border", ruleset.border),
+            ("shadow", ruleset.shadow),
+            ("rounding", ruleset.rounding),
+            ("decorate", ruleset.decorate),
+            ("persistent", ruleset.persistent),
+        ]
+    );
 
     vector.join(",")
 }
